c5de2c5046a3026d1978c1636a9b5d05e077dc61
Tyraka <42555839+Tyraka@users.noreply.github.com>
2018-09-05 04:05:42 +0200

    Update reciver.ino

--- reciver.ino
+++ reciver.ino
@@ -3,64 +3,55 @@
 #define RXADDR 0x586F2E10 // Address of this device
 #define TXADDR 0xFE4CA6E5 // Address of device to send to
 
-#define PACKET_NONE    0
-#define PACKET_OK   1
-#define PACKET_INVALID  2
+#define PACKET_NONE		0
+#define PACKET_OK		1
+#define PACKET_INVALID	2
 
 static volatile uint8_t packetStatus;
 
 void NRF905_CB_RXCOMPLETE(void)
 {
-  packetStatus = PACKET_OK;
-  nRF905_standby();
+	packetStatus = PACKET_OK;
+	nRF905_standby();
 }
 
 void NRF905_CB_RXINVALID(void)
 {
-  packetStatus = PACKET_INVALID;
-  nRF905_standby();
+	packetStatus = PACKET_INVALID;
+	nRF905_standby();
 }
 
 void setup()
 {
-  //Set up the serial porta
-  Serial.begin(115200);
-  Serial.println(F("Server started"));
+	Serial.begin(115200);
+	Serial.println(F("Server started"));
 
-  // Set address of this device
-  nRF905_setListenAddress(RXADDR);
+	// Start up
+	nRF905_init();
+	
+	// Set address of this device
+	nRF905_setListenAddress(RXADDR);
 
-  // Put into receive mode
-  nRF905_RX();
+	// Put into receive mode
+	nRF905_RX();
 }
 
-void loop() 
+void loop()
 {
-  // Send the data for engine start
-  if(Serial.available() > 0){
-    char* command; 
-    sprintf(command, "%d", Serial.read());
-    int i;
-    for(i = 0; i < 3; i++){
-      while(!nRF905_TX(TXADDR, command, sizeof(command), NRF905_NEXTMODE_RX));
-    }
-  }
-  
-  // Wait for data
-  while(packetStatus == PACKET_NONE);
+	while(packetStatus == PACKET_NONE);
 
-  if(packetStatus == PACKET_OK)
-  {
-    //Reset package status
-    packetStatus = PACKET_NONE;
+		packetStatus = PACKET_NONE;
 
-    // Make buffer for data
-    uint8_t buffer[NRF905_MAX_PAYLOAD];
-    nRF905_read(buffer, sizeof(buffer));
+		// Make buffer for data
+		uint8_t buffer[NRF905_MAX_PAYLOAD];
+		nRF905_read(buffer, sizeof(buffer));
 
-    //Print the data
-    Serial.write(buffer, sizeof(buffer));
-  }
+		// Send back the data, once the transmission has completed go into receive mode
+		while(!nRF905_TX(TXADDR, buffer, sizeof(buffer), NRF905_NEXTMODE_RX));
+
+		// Print out ping contents
+		Serial.print(F("Data from server: "));
+		Serial.write(buffer, sizeof(buffer));
+		Serial.println();
+
 }
-
-
